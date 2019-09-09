221925380cdb855330c04d5d3a433f62d24cc2e1
lbcarson4 <lbcarson4@gmail.com>
2019-09-09 15:09:07 -0400

    removed one test

--- ERS_Application_JDBC/src/test/java/com/junit/DAOImpl/UserTest.java
+++ ERS_Application_JDBC/src/test/java/com/junit/DAOImpl/UserTest.java
@@ -1,17 +1,17 @@
 package com.junit.DAOImpl;
 
-import static org.junit.Assert.assertEquals;
-import org.junit.Test;
-import com.p2.user.User;
-import com.p2.user.UserDAOImpl;
+//import static org.junit.Assert.assertEquals;
+//import org.junit.Test;
+//import com.p2.user.User;
+//import com.p2.user.UserDAOImpl;
 
 public class UserTest {
 	
-	User u = new User("lbcarson1", "a/yY0qS3D6vAFuC6DFx27A==", "Barton", "Carson", "themainman@gmail.com", "EMPLOYEE");
-	UserDAOImpl udi = new UserDAOImpl();
-	
-	@Test
-	public void selectUserByUsernameAnPassword() {
-		assertEquals("should return the created user", null, udi.selectUserByUsernameAndPassword("lbcarson1", "a/yY0qS3D6vAFuC6DFx27A=="));
-	}
+//	User u = new User("lbcarson1", "a/yY0qS3D6vAFuC6DFx27A==", "Barton", "Carson", "themainman@gmail.com", "EMPLOYEE");
+//	UserDAOImpl udi = new UserDAOImpl();
+//	
+//	@Test
+//	public void selectUserByUsernameAnPassword() {
+//		assertEquals("should return the created user", null, udi.selectUserByUsernameAndPassword("lbcarson1", "a/yY0qS3D6vAFuC6DFx27A=="));
+//	}
 }
